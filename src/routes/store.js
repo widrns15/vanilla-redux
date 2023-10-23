ad094ae235856d6d3d4139a469529bcee9715acb
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-10-24 03:20:44 +0900

    ♻️ refactor: createSlice로 기존 state, reducer 리팩토링

--- src/routes/store.js
+++ src/routes/store.js
@@ -1,17 +1,20 @@
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-export const addToDo = createAction("ADD");
-export const deleteToDo = createAction("DELETE");
-
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
-  },
-  [deleteToDo]: (state, action) => {
-    return state.filter((toDo) => toDo.id !== action.payload);
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    addToDo: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    deleteToDo: (state, action) => {
+      return state.filter((toDo) => toDo.id !== action.payload);
+    },
   },
 });
 
-const store = configureStore({ reducer });
+const store = configureStore({ reducer: toDos.reducer });
+
+export const { addToDo, deleteToDo } = toDos.actions;
 
 export default store;
